290ded17cc484c3368143ddc1b175f292c399760
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-19 23:59:19 +0000

    added route for to delete urls

--- expressServer.js
+++ expressServer.js
@@ -62,5 +62,14 @@ app.post("/urls", (req, res) => {
  res.redirect(`/urls/${shortURL}`) 
 });
 
+app.post("/urls/:shortURL/delete", (req,res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  delete urlDatabase[shortURL];
+  res.redirect("/urls")
+});
+
+
+
 //To use nodemon: type npm start or ./node_modules/.bin/nodemon -L expressServer.js 
 //website http://localhost:8080/urls
